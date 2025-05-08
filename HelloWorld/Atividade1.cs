03769eaa087ba3ad353d3592f53ad868ef6fcd13
Gabriel Nascimento Barbosa <gabrielnascimentoph2@gmail.com>
2025-05-08 11:05:40 -0300

    Add files via upload

---
+++ HelloWorld/Atividade1.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld;
+
+class Atividade1
+{
+    public void LerNome()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+    }
+
+    
+}
